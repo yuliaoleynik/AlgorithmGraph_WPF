1d5af191ff12730f908396eb721693de0c522e55
Yulia Oleynik <51270802+yuliaoleynik@users.noreply.github.com>
2022-06-26 13:58:58 +0300

    Add files via upload

---
+++ Algorithms/Dijkstra.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmGraph.Algorithms
+{
+    class Dijkstra
+    {
+        public Dijkstra(int[,] matrix, int s)
+        {
+            Initial(0, s);
+            while (queue.Count > 0)
+            {
+                int u = getNextVertex();
+
+                for (int i = 0; i < s; i++)
+                {
+                    if (matrix[u, i] > 0)
+                    {
+                        if (dist[i] > dist[u] + matrix[u, i])
+                        {
+                            dist[i] = dist[u] + matrix[u, i];
+                        }
+                    }
+                }
+            }
+        }
+
+        public double[] dist { get; set; }
+        int getNextVertex()
+        {
+            var min = double.PositiveInfinity;
+            int vertex = -1;
+
+            foreach (int val in queue)
+            {
+                if (dist[val] <= min)
+                {
+                    min = dist[val];
+                    vertex = val;
+                }
+            }
+            queue.Remove(vertex);
+            return vertex;
+        }
+
+        List<int> queue = new List<int>();
+        public void Initial(int s, int len)
+        {
+            dist = new double[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                dist[i] = double.PositiveInfinity;
+                queue.Add(i);
+            }
+            dist[0] = 0;
+        }
+    }
+}
+
---
+++ Algorithms/Floyd–Warshall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmGraph.Algorithms
+{
+     public class Floyd_Warshall
+    {
+		public double[,] distance { get; set; }
+
+        public Floyd_Warshall(int[,] matrix, int count)
+        {
+			distance = new double[count, count];
+
+			for (int i = 0; i < count; ++i)
+				for (int j = 0; j < count; ++j)
+				{
+					if (i != j && matrix[i, j] == 0)
+					{
+						distance[i, j] = double.PositiveInfinity;
+					}
+					else { distance[i, j] = matrix[i, j]; }
+				}
+
+			for (int k = 0; k < count; ++k)
+			{
+				for (int i = 0; i < count; ++i)
+				{
+					for (int j = 0; j < count; ++j)
+					{
+						if (distance[i, k] + distance[k, j] < distance[i, j])
+							distance[i, j] = distance[i, k] + distance[k, j];
+					}
+				}
+			}
+		}
+    }
+}
