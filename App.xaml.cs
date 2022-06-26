df7c7eabe3a489889a33dec3d7dc4018b28f49af
Yulia Oleynik <51270802+yuliaoleynik@users.noreply.github.com>
2022-06-26 14:01:07 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AlgorithmGraph
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Create the ViewModel and expose it using the View's DataContext
+            Views.MainWindow view = new Views.MainWindow();
+            view.DataContext = new ViewModels.MainViewModel();
+            view.Show();
+        }
+    }
+}
